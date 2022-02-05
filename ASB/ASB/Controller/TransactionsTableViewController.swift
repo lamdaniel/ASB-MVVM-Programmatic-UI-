f5c3c11fbd5c50ee0ca9f3e6892b4478e51c9097
Daniel Lam <pickypickleapp@gmail.com>
2022-02-05 21:55:47 +1300

    Changed to TableViewController, fixed bug

--- ASB/ASB/Controller/TransactionsTableViewController.swift
+++ ASB/ASB/Controller/TransactionsTableViewController.swift
@@ -9,20 +9,15 @@ import UIKit
 
 private let reuseIdentifier = "TransactionsCell"
 
-class TransactionsViewController: UIViewController {
-    
-    private let tableView: UITableView = {
-        let tableView = UITableView()
+class TransactionsTableViewController: UITableViewController {
         
-        return tableView
-    }()
-    
     private let transactionsViewModel = TransactionViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureUI()
+        configureNavigationBar()
         configureTableView()
         callToViewModelForTransactions()
     }
@@ -30,13 +25,14 @@ class TransactionsViewController: UIViewController {
     private func configureUI() {
         view.backgroundColor = .white
         
-        view.addSubview(tableView)
-        tableView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor)
+    }
+    
+    private func configureNavigationBar() {
+        title = "Transactions"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
     
     private func configureTableView() {
-        tableView.dataSource = self
-        tableView.delegate = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
     }
 
@@ -49,22 +45,14 @@ class TransactionsViewController: UIViewController {
         
         transactionsViewModel.callFuncToGetTransactionData()
     }
-}
-
-extension TransactionsViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         transactionsViewModel.transactions.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
         cell.textLabel?.text = transactionsViewModel.transactions[indexPath.row].summary
         return cell
     }
-    
-    
-}
-
-extension TransactionsViewController: UITableViewDelegate {
-    
 }
